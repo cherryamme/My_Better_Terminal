48153b50526d085927e0c20fc453c0555d148271
cherryamme <cherryamme@qq.com>
2022-08-06 02:24:24 +0800

    🧐install command

--- AllMy_config/install.sh
+++ AllMy_config/install.sh
@@ -1,2 +1,2 @@
 
-ln -sf $HOME/My_Better_Terminal/AllMy_config/{.*} ~
+ln -sf $HOME/My_Better_Terminal/AllMy_config/{.Rprofile,.bashrc,.fzf.bash,.fzf.zsh,.p10k.zsh,.radian_profile,.rgignore,.tmux.conf,.tmux.conf.local,.zshrc} ~
